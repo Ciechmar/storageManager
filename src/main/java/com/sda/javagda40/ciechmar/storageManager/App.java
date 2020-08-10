01ff895a5538494a16293050a8d581f23a29f32a
Ciechmar <Ciechmar@o2.pl>
2020-08-10 19:16:07 +0200

    Initial commit

---
+++ src/main/java/com/sda/javagda40/ciechmar/storageManager/App.java
@@ -0,0 +1,13 @@
+package com.sda.javagda40.ciechmar.storageManager;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Initial version.");
+
+
+
+
+
+    }
+}
