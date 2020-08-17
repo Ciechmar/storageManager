3e3a04092c8a62017d12e04974c43088b52d97a4
Ciechmar <Ciechmar@o2.pl>
2020-08-17 22:02:51 +0200

    adding parsers: delete, add, find, findById

--- src/main/java/com/sda/javagda40/ciechmar/storageManager/App.java
+++ src/main/java/com/sda/javagda40/ciechmar/storageManager/App.java
@@ -2,8 +2,12 @@ package com.sda.javagda40.ciechmar.storageManager;
 
 import com.sda.javagda40.ciechmar.storageManager.database.EntityDao;
 import com.sda.javagda40.ciechmar.storageManager.database.HibernateUtil;
+import com.sda.javagda40.ciechmar.storageManager.model.Address;
 import com.sda.javagda40.ciechmar.storageManager.model.AppUser;
+import com.sda.javagda40.ciechmar.storageManager.model.CompanyData;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class App {
@@ -25,9 +29,81 @@ public class App {
             } else if (words[0].equalsIgnoreCase("user") &&
                     words[1].equalsIgnoreCase("add")) {
                 handleAdduser(words);
+            } else if (words[0].equalsIgnoreCase("user") &&
+                    words[1].equalsIgnoreCase("delete")) {
+                handleDeleteUser(words);
+            } else if (words[0].equalsIgnoreCase("user") &&
+                    words[1].equalsIgnoreCase("find")) {
+                handleFindUser(words);
             }
 
         } while (!command.equalsIgnoreCase("end"));
+
+    }
+
+    // Na razie zwraca na ekran, w przyszłości chcę by zwracało AppUser'a, by móc wykorzystać tę metodę w metodzie handleDeleteUser()
+    private static void handleFindUser(String[] words) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Szukaj po: \n1.Nazwisko\n2.Telefon\n3.Id");
+        String answer = scanner.nextLine();
+
+        switch (answer.toLowerCase()) {
+            case "1":
+            case "nazwisko": {
+                EntityDao<AppUser> userEntityDao = new EntityDao<>();
+                List<AppUser> usersList = userEntityDao.findAll(AppUser.class);
+                System.out.println("Podaj nazwisko:");
+                String nameString = scanner.nextLine();
+                for (AppUser user : usersList) {
+                    if (user.getLastName().equalsIgnoreCase(nameString)) {
+                        System.out.println(user);
+                    }
+                }
+                break;
+            }
+            case "2":
+            case "telefon": {
+                EntityDao<AppUser> userEntityDao = new EntityDao<>();
+                List<AppUser> usersList = userEntityDao.findAll(AppUser.class);
+                System.out.println("Podaj numer telefonu:");
+                String telNumber = scanner.nextLine();
+                for (AppUser user : usersList) {
+                    if (user.getPhoneNumber().equals(telNumber)) {
+                        System.out.println(user);
+                    }
+                }
+                break;
+            }
+
+            case "3":
+            case "id": {
+                EntityDao<AppUser> userEntityDao = new EntityDao<>();
+                System.out.println("Podaj id:");
+                Long id = scanner.nextLong();
+                userEntityDao.findById(AppUser.class, id).ifPresent(System.out::println);
+                break;
+            }
+        }
+
+    }
+
+    private static void handleDeleteUser(String[] words) {
+        System.out.println("Czy znasz ID użytkownika, którego chcesz usunąć z bazy danych? T/N");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextLine().equalsIgnoreCase("T")) {
+            System.out.println("Podaj ID użytkownia:");
+            Long id = scanner.nextLong();
+            EntityDao<AppUser> userEntityDao = new EntityDao<>();
+            userEntityDao.findById(AppUser.class, id).ifPresent(user-> userEntityDao.delete(user));
+        }else {
+            handleFindUser("find user".split(" "));
+            System.out.println("Podaj ID użytkownia:");
+            Long id = scanner.nextLong();
+            EntityDao<AppUser> userEntityDao = new EntityDao<>();
+            userEntityDao.findById(AppUser.class, id).ifPresent(user-> userEntityDao.delete(user));
+        }
+
 
     }
 
@@ -35,7 +111,6 @@ public class App {
         EntityDao<AppUser> appUserEntityDao = new EntityDao<>();
         AppUser appUser = new AppUser();
         Scanner scanner = new Scanner(System.in);
-//        String answer = scanner.nextLine();
         System.out.println("Podaj dane nowego użytkownika:");
         System.out.println("Imie:");
         appUser.setFirstName(scanner.nextLine());
@@ -45,12 +120,46 @@ public class App {
         appUser.setPhoneNumber(scanner.nextLine());
         System.out.println("Adres kontaktowy email:");
         appUser.setEmail(scanner.nextLine());
-        System.out.println("Czy jest to klient prywatny/ indywidualny? (T/N)");
+        System.out.println("Czy jest to klient firma? (T/N)");
         if (scanner.nextLine().equalsIgnoreCase("t")) {
             appUser.setCompany(true);
-        } else appUser.setCompany(false);
+            handleAddAdress();
+            handleCompanyData();
 
+        } else {
+            appUser.setCompany(false);
+            handleAddAdress();
+        }
         appUserEntityDao.saveOrUpdate(appUser);
+    }
+
+    private static void handleCompanyData() {
+        Scanner scanner = new Scanner(System.in);
+        CompanyData companyData = new CompanyData();
+        EntityDao<CompanyData> entityDao = new EntityDao<>();
+        System.out.println("Podaj dane firmy:");
+        System.out.println("Nazwa firmy");
+        companyData.setCompanyName(scanner.nextLine());
+        System.out.println("NIP:");
+        companyData.setNIP(scanner.nextLine());
+        entityDao.saveOrUpdate(companyData);
+    }
+
+    private static void handleAddAdress() {
+        Scanner scanner = new Scanner(System.in);
+        Address address = new Address();
+        EntityDao<Address> entityDao = new EntityDao<>();
+        System.out.println("Podaj dane adresowe użytkownika:");
+        System.out.println("Miasto");
+        address.setCity(scanner.nextLine());
+        System.out.println("Kod pocztowy");
+        address.setPostCode(scanner.nextLine());
+        System.out.println("ulica:");
+        address.setStreet(scanner.nextLine());
+        System.out.println("Numer budynku:");
+        address.setBuildingNumber(scanner.nextLine());
+        entityDao.saveOrUpdate(address);
+
     }
 
     private static void handleListUser(String[] words) {
